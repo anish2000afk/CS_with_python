7b26f35961f009b1de8b8b055358a5d8087894e6
Anish <anishkandulna@gmail.com>
2024-02-22 21:21:34 +0530

    update

---
+++ Python_Fundamentals/C_1.py
@@ -0,0 +1,6 @@
+"""Program displays a joke.
+Only only if the user presses the Enter Key"""
+
+joke = "What is love? Baby don't hurt me"
+word = input("Press Enter to hear a joke!! ")
+print(joke)
---
+++ Python_Fundamentals/C_14.py
@@ -0,0 +1,4 @@
+x = int(input("Enter a number:  "))
+y = x + 1
+z = x + 2
+print(x, y, z, sep="")
