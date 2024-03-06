db705487057ab3dc43b47b7bef0916bf489f1f5b
Anish <anishkandulna@gmail.com>
2024-03-06 07:58:52 +0530

    testing test.py

--- Python_Fundamentals/test.py
+++ Python_Fundamentals/test.py
@@ -1,1 +1,22 @@
-print(print('hola'))
+a = ''
+b = ' '
+c = 'great'
+d = 'Great'
+e = 'GREAT'
+f = 'Great'
+g = ' GREAT'
+h = ' great'
+x = 29.0
+y = 13
+z = 2
+p = 0.5
+q = 12.79
+i = 13
+j = 29.0
+k = y - 11
+
+print(y > p * 26)
+# False
+print((y > p ) * 26)
+print(x <= y < x + y)
+
