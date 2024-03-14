6478e2305896ea57429469450fa6cf35a8571b47
Anish <anishkandulna@gmail.com>
2024-03-14 15:56:39 +0530

    Solving Type C questions

---
+++ Python_Fundamentals/Chapter_7_examples/TypeC_4.py
@@ -0,0 +1,5 @@
+# Conversion of seconds to minutes.
+secs = int(input('Enter seconds : '))
+min = secs // 60
+seconds = secs % 60
+print(" The time is ", min , "mins ", seconds , "seconds")
--- Python_Fundamentals/Chapter_7_examples/TypeC_7.py
+++ Python_Fundamentals/Chapter_7_examples/TypeC_7.py
@@ -1,3 +1,4 @@
+
 x = input("Enter a 2 digit number: ")
 y = (x[-1] + x[-2])
 print(x, y)
