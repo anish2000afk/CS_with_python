28b9f64d326a53a4c76f9cbfc56414704d177edb
Anish <anishkandulna@gmail.com>
2024-07-15 12:14:03 +0530

    new

--- Python_Fundamentals/test.py
+++ Python_Fundamentals/test.py
@@ -1,7 +1,1 @@
-import statistics as st
-
-v = [7, 8, 8, 11, 7, 7]
-m1 = st.mean(v)
-m2 = st.mode(v)  # most frequent; remember to rearrange
-m3 = st.median(v)  # most central values
-print(m1, m2, m3)
+print("a" + 1 > 1)
